3d7168909faf6812d6fff3f784201803254eb968
Tharit Tiewjarernsopa <twistycsdev@gmail.com>
2021-01-15 16:17:53 +0700

    add imp get user by id

--- imp/user_interface.go
+++ imp/user_interface.go
@@ -5,6 +5,7 @@ import "github.com/twisty/clean-arch/models"
 type UserService interface {
 	GetAllUser() (userList []models.User, err error)
 	GetUserById(id string) (user models.User, err error)
+	GetUserByUserId(userId string) (user models.User, err error)
 	InsertUser(u *models.User) (err error)
 	UpdateUser(u *models.User, id string) (err error)
 	DeleteUserById(id string) (err error)
@@ -13,6 +14,7 @@ type UserService interface {
 type UserRepository interface {
 	GetAllUser(u *[]models.User) (err error)
 	GetUserById(u *models.User, id string) (err error)
+	GetUserByUserId(u *models.User, userId string) (err error)
 	InsertUser(u *models.User) (err error)
 	UpdateUser(u *models.User) (err error)
 	DeleteUserById(id string) (err error)
