654ff693955776e86cc259f1638151e043f9c85b
Tharit Tiewjarernsopa <twistycsdev@gmail.com>
2021-01-15 11:59:18 +0700

    add update controller

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -1,8 +1,9 @@
 package controller
 
 import (
-	"log"
 	"net/http"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/gin-gonic/gin"
 	"github.com/twisty/clean-arch/imp"
@@ -21,6 +22,7 @@ func UserControllerInit(userService imp.UserService) *UserController {
 	}
 }
 func (u *UserController) GetAllUserController(c *gin.Context) {
+	log.Info("Get All USER !!!")
 	resp, err := u.userService.GetAllUser()
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
@@ -34,7 +36,7 @@ func (u *UserController) GetUserByIdController(c *gin.Context) {
 	resp, err := u.userService.GetUserById(id)
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
-	} else if len(resp) < 1 {
+	} else if (models.User{} == resp) {
 		c.JSON(http.StatusNotFound, "ID : "+id+" Not Found")
 	} else {
 		c.JSON(http.StatusOK, resp)
@@ -55,5 +57,32 @@ func (u *UserController) InsertUserController(c *gin.Context) {
 		response.StatusCode = http.StatusCreated
 		response.StatusMsg = "Created"
 		c.JSON(http.StatusCreated, response)
+	}
+}
+
+func (u *UserController) UpdateUserController(c *gin.Context) {
+	var jsonInputUser models.User
+	id := c.Params.ByName("id")
+	if err := c.ShouldBindJSON(&jsonInputUser); err != nil {
+		log.Error("Err : ", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	} else {
+		log.Info("[UPDATE CONTROLLER] User Input Information : ", jsonInputUser)
+		resp, err := u.userService.GetUserById(id)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, err)
+		} else if (models.User{} == resp) {
+			response.StatusCode = http.StatusNotFound
+			response.StatusMsg = "ID : " + id + " Not Found"
+			c.JSON(http.StatusNotFound, response)
+		} else {
+			if err := u.userService.UpdateUser(&jsonInputUser, id); err != nil {
+				log.Error("Err : ", err)
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			}
+			response.StatusCode = http.StatusAccepted
+			response.StatusMsg = "Updated"
+			c.JSON(http.StatusOK, response)
+		}
 	}
 }
