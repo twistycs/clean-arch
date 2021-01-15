1367ad9758aede6d218439b6e14238d19208ee66
Tharit Tiewjarernsopa <twistycsdev@gmail.com>
2021-01-15 11:01:06 +0700

    add imp order

---
+++ imp/order_interface.go
@@ -0,0 +1,11 @@
+package imp
+
+import "github.com/twisty/clean-arch/models"
+
+type OrderService interface {
+	GetAllOrder() (orderList []models.Order, err error)
+}
+
+type OrderRepository interface {
+	GetAllOrder(o *[]models.Order) (err error)
+}
