769a5fab760e5ef923c21fc4d82a7eec82517b88
Tharit Tiewjarernsopa <twistycsdev@gmail.com>
2021-01-15 10:58:33 +0700

    add service user

---
+++ services/user_service.go
@@ -0,0 +1,50 @@
+package services
+
+import (
+	"fmt"
+
+	"github.com/twisty/clean-arch/imp"
+	"github.com/twisty/clean-arch/models"
+)
+
+type userService struct {
+	userRepo imp.UserRepository
+}
+
+func NewService(repo imp.UserRepository) imp.UserService {
+	return &userService{userRepo: repo}
+}
+
+func (userService *userService) GetAllUser() (users []models.User, err error) {
+	var user []models.User
+	handle := userService.userRepo.GetAllUser(&user)
+	if handle != nil {
+		fmt.Println("Error")
+	}
+	return user, handle
+}
+
+func (userService *userService) GetUserById(id string) (users []models.User, err error) {
+	var user []models.User
+	handle := userService.userRepo.GetUserById(&user, id)
+	if handle != nil {
+		fmt.Println("Error")
+	}
+	return user, handle
+}
+
+func (userService *userService) InsertUser(user *models.User) (err error) {
+	handle := userService.userRepo.InsertUser(user)
+	if handle != nil {
+		fmt.Println("Error")
+	}
+	return handle
+}
+
+func (userService *userService) UpdateUser(user *models.User) (err error) {
+	handle := userService.userRepo.UpdateUser(user)
+	if handle != nil {
+		fmt.Println("Error")
+	}
+	return handle
+}
