1c48f020cfdd7e88ffe19a6ddb895f5968a4f0ad
Tharit Tiewjarernsopa <twistycsdev@gmail.com>
2021-01-15 11:00:58 +0700

    add repo order

---
+++ repositories/order_repository.go
@@ -0,0 +1,24 @@
+package repositories
+
+import (
+	"github.com/twisty/clean-arch/imp"
+	"github.com/twisty/clean-arch/models"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+type orderRepository struct {
+	connect *gorm.DB
+}
+
+func RepositoriesOrder(connect *gorm.DB) imp.OrderRepository {
+	return &orderRepository{connect}
+}
+
+func (repo *orderRepository) GetAllOrder(o *[]models.Order) (err error) {
+	// if err = repo.connect.Preload(clause.Associations).Find(o).Error; err != nil {
+	if err = repo.connect.Preload(clause.Associations).Find(o).Error; err != nil {
+		return err
+	}
+	return nil
+}
