be7f9f61b0eac6cb9d2502ee81b6c64cd1825a70
Tharit Tiewjarernsopa <twistycsdev@gmail.com>
2021-01-15 10:58:11 +0700

    add user repository

---
+++ repositories/user_repository.go
@@ -0,0 +1,43 @@
+package repositories
+
+import (
+	"github.com/twisty/clean-arch/imp"
+	"github.com/twisty/clean-arch/models"
+	"gorm.io/gorm"
+)
+
+type repository struct {
+	connect *gorm.DB
+}
+
+func RepositoriesConstruct(connect *gorm.DB) imp.UserRepository {
+	return &repository{connect}
+}
+
+func (repo *repository) GetAllUser(u *[]models.User) (err error) {
+	if err = repo.connect.Find(u).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repo *repository) GetUserById(u *[]models.User, id string) (err error) {
+	if err = repo.connect.Where("id = ?", id).Find(u).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repo *repository) InsertUser(u *models.User) (err error) {
+	if err = repo.connect.Create(u).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repo *repository) UpdateUser(u *models.User) (err error) {
+	if err = repo.connect.Save(u).Error; err != nil {
+		return err
+	}
+	return nil
+}
